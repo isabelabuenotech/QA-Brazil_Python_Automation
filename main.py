465b008beb6d5152ba7c78a7d2911b8c0e117bd9
isabelabuenotech <isabelabueno.tech@gmail.com>
2026-07-15 21:53:14 -0300

    Código

--- main.py
+++ main.py
@@ -1,2 +1,53 @@
-
+import data
 class TestUrbanRoutes:
+        def test_set_route(self):
+            # Adicionar em S8
+            print("função criada para definir a rota")
+            pass
+
+        def test_select_plan(self):
+            # Adicionar em S8
+            print("função criada para selecionar o plano")
+            pass
+
+        def test_fill_phone_number(self):
+            # Adicionar em S8
+            print("função criada para preencher o número de telefone")
+            pass
+
+        def test_fill_card(self):
+            # Adicionar em S8
+            print("função criada para preencher o cartão")
+            pass
+
+        def test_comment_for_driver(self):
+            # Adicionar em S8
+            print("função criada para adicionar comentário ao motorista")
+            pass
+
+        def test_order_blanket_and_handkerchiefs(self):
+            # Adicionar em S8
+            print("função criada para pedir cobertor e lenços")
+            pass
+
+        def test_order_2_ice_creams(self):
+            # Adicionar em S8
+            print("função criada para pedir 2 sorvetes")
+            for iterar in range(2):
+                #Adicionar em S8
+            pass
+
+        def test_car_search_model_appears(self):
+            # Adicionar em S8
+            print("função criada para verificar se o campo de pesquisa de modelo do carro aparece")
+            pass
+
+import helpers
+class TestUrbanRoutes:
+        @classmethod
+        def setUpClass(cls):
+            if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+                print("Conectado ao servidor Urban Routes")
+            else:
+                print(
+                    "Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
